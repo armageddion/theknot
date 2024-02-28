b24216a40444aff9dce1b5206138fc202d8d0436
Goran <gospaso@gmail.com>
2024-02-27 22:42:51 -0400

    Add infinite scroll loop, update layout, remove unused
    ..update dependencies

--- src/components/The/Header.vue
+++ src/components/The/Header.vue
@@ -16,20 +16,18 @@ async function toggleLocales() {
     flex="~ gap-4"
     fixed left-0 right-0 top-0 mb-8 px-8 py-4 text-xl
   >
-    <TheCountdown />
-
     <span flex-auto />
 
-    <button icon-btn :title="t('button.toggle_dark')" @click="toggleDark()">
+    <button
+      icon-btn
+      :title="isDark ? t('button.toggle_dark_off') : t('button.toggle_dark_on')"
+      @click="toggleDark()"
+    >
       <div i="carbon-sun dark:carbon-moon" />
     </button>
 
     <button icon-btn :title="t('button.toggle_langs')" @click="toggleLocales()">
       <div i-carbon-language />
     </button>
-
-    <RouterLink icon-btn to="/not-a-page" :title="t('button.about')" data-test-id="about">
-      <div i-carbon-dicom-overlay />
-    </RouterLink>
   </nav>
 </template>
