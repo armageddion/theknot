5324ae0cbf7790dcee10db6da1494d3921de8357
Goran <gospaso@gmail.com>
2024-02-27 22:42:51 -0400

    Add infinite scroll loop, update layout, remove unused
    ..update dependencies

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,60 +1,113 @@
 <script setup lang="ts">
-import TheSlide from '~/components/The/Slide.vue'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import Lenis from '@studio-freight/lenis'
+import markdownit from 'markdown-it'
 
 defineOptions({
   name: 'IndexPage',
 })
 
 const { t } = useI18n()
-const slide = ref<HTMLElement>()
-const { style } = useDraggable(slide, {
-  initialValue: { x: 80, y: 400 },
-  preventDefault: true,
-  stopPropagation: true,
-  capture: false,
-  containerElement: document.body,
+
+gsap.registerPlugin(ScrollTrigger)
+const md = markdownit()
+
+const lenis = new Lenis({
+  wrapper: document.body,
+  smoothWheel: true,
+  infinite: true,
 })
+
+onMounted(() => {
+  requestAnimationFrame(raf)
+
+  const refresh = () => {
+    ScrollTrigger.clearScrollMemory()
+    window.history.scrollRestoration = 'manual'
+    ScrollTrigger.refresh(true)
+  }
+
+  refresh()
+  window.addEventListener('resize', refresh)
+})
+
+lenis.on('scroll', () => {
+  ScrollTrigger.update()
+})
+
+function raf(time: number) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
 </script>
 
 <template>
   <section min-h-screen flex-center>
-    <h1 text-xl font-bold>
-      {{ t('intro.title') }}
-    </h1>
-    <div
-      ref="slide"
-      :style
-      absolute
-      select-none
-    >
-      <TheSlide w-xl />
-    </div>
-    <div absolute-center top="[30%]">
-      <RouterLink to="/rings">
-        <RingOne />
-      </RouterLink>
+    <div>
+      <h1 text="[4rem]" font-bold>
+        {{ t('meta.title') }}
+      </h1>
+      <p max-w-md>
+        {{ t('meta.description') }}
+      </p>
     </div>
   </section>
+
   <section min-h-screen flex-center>
-    <h1 text-xl font-bold>
-      {{ t('intro.where') }}
-    </h1>
+    <div>
+      <a href="#where">
+        <h2 id="where" my-3 text-4xl font-bold>
+          {{ t('where.title') }}
+        </h2>
+      </a>
+      <div max-w-md prose v-html="md.render(t('where.description'))" />
+    </div>
   </section>
+
   <section min-h-screen flex-center>
-    <h1 text-xl font-bold>
-      {{ t('intro.when') }}
-    </h1>
+    <div>
+      <a href="#when">
+        <h2 id="when" my-3 text-4xl font-bold>
+          {{ t('when.title') }}
+        </h2>
+      </a>
+      <div max-w-md prose v-html="md.render(t('when.description'))" />
+    </div>
   </section>
+
   <section min-h-screen flex-center>
-    <h1 text-xl font-bold>
-      {{ t('intro.give') }}
-    </h1>
+    <div>
+      <a href="#what">
+        <h2 id="what" my-3 text-4xl font-bold>
+          {{ t('what.title') }}
+        </h2>
+      </a>
+      <div max-w-md prose v-html="md.render(t('what.description'))" />
+    </div>
   </section>
-  <footer flex-center py-12>
-    <h1 text-xl font-bold>
-      {{ t('intro.end') }}
-    </h1>
-  </footer>
+
+  <section min-h-screen flex-center>
+    <div>
+      <a href="#gift">
+        <h2 id="gift" my-3 text-4xl font-bold>
+          {{ t('gift.title') }}
+        </h2>
+      </a>
+      <div max-w-md prose v-html="md.render(t('gift.description'))" />
+    </div>
+  </section>
+
+  <section min-h-screen flex-center>
+    <div>
+      <h1 text="[4rem]" font-bold>
+        {{ t('meta.title') }}
+      </h1>
+      <p max-w-md>
+        {{ t('meta.description') }}
+      </p>
+    </div>
+  </section>
 </template>
 
 <route lang="yaml">
