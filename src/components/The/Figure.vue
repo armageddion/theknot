4ef550607727cef0e523a99ddbcc62fb6c4fe718
Goran <gospaso@gmail.com>
2024-02-28 21:56:20 -0400

    Add images and scroll transitions

---
+++ src/components/The/Figure.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const props = defineProps<{
+  src: string
+}>()
+
+// scale from small to original size
+const outerStyle = computed(() => {
+  const isLeft = Math.random() > 0.5
+  const scale = ref(1)
+  return {
+    transformOrigin: isLeft ? '0% 0%' : '100% 0%',
+    transform: `scale(${scale.value})`,
+  }
+})
+
+// scale from small to original size
+const innerStyle = computed(() => {
+  const isLeft = Math.random() > 0.5
+  const scale = ref(1)
+  return {
+    backgroundImage: `url(${props.src})`,
+    transformOrigin: isLeft ? '0% 0%' : '100% 0%',
+    transform: `scale(${scale.value})`,
+  }
+})
+</script>
+
+<template>
+  <figure overflow-hidden>
+    <div :style="outerStyle" h-full w-full overflow-hidden>
+      <span :style="innerStyle" block h-full w-full bg-cover bg-center />
+    </div>
+  </figure>
+</template>
+
+<style scoped>
+figure {
+  grid-column: var(--c) / span var(--s);
+  grid-row: var(--r);
+  position: relative;
+}
+</style>
