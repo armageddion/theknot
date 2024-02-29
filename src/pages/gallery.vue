850c40406fe9e620004309b4303234fce9cfecdf
Goran <gospaso@gmail.com>
2024-02-28 22:03:21 -0400

    Add image filters, rounding, link to gallery
    - add gallery page
    - update colors... idk

---
+++ src/pages/gallery.vue
@@ -0,0 +1,10 @@
+<template>
+  <section h-screen flex-center flex-col gap-4>
+    <div text-center prose>
+      <p text-lg>
+        Come back here after the event to view everyone's photos from the day and to upload your own.
+      </p>
+    </div>
+    <TheSlide aspect-video w-full max-w="65vw" />
+  </section>
+</template>
