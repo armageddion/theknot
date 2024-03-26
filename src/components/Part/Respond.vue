f3c646ad7703a84f2fdd25894215b0a95bb44ecb
Goran <gospaso@gmail.com>
2024-03-26 16:17:50 -0400

    split parts into components

---
+++ src/components/Part/Respond.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import markdownit from 'markdown-it'
+
+const { t } = useI18n()
+const md = markdownit()
+</script>
+
+<template>
+  <div min-h-80>
+    <h2 my-3 text-4xl text-secondary>
+      {{ t('rsvp.title') }}
+    </h2>
+    <div max-w-md prose v-html="md.render(t('rsvp.description'))" />
+  </div>
+</template>
---
+++ src/components/Part/Welcome.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import markdownit from 'markdown-it'
+
+const { t } = useI18n()
+const md = markdownit()
+</script>
+
+<template>
+  <div>
+    <h2 my-3 text-4xl text-secondary>
+      {{ t('welcome.title') }}
+    </h2>
+    <div max-w-md prose v-html="md.render(t('welcome.description'))" />
+  </div>
+</template>
