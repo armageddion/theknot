1755c2f2727a6a94695f58a75bb900b571913926
Goran <gospaso@gmail.com>
2024-03-28 14:38:05 -0400

    fix build & lint

--- scripts/latn-cyrl.js
+++ scripts/latn-cyrl.js
@@ -3,18 +3,17 @@ import { dirname, join } from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { parse, stringify } from 'yaml'
 
-const map = { Lj: 'Љ', lj: 'љ', Nj: 'Њ', nj: 'њ', Dž: 'Џ', dž: 'џ', A: 'А', a: 'а', B: 'Б', b: 'б', V: 'В', v: 'в', G: 'Г', g: 'г', D: 'Д', d: 'д', Đ: 'Ђ', đ: 'ђ', E: 'Е', e: 'е', Ž: 'Ж', ž: 'ж', Z: 'З', z: 'з', I: 'И', i: 'и', J: 'Ј', j: 'ј', K: 'К', k: 'к', L: 'Л', l: 'л', M: 'М', m: 'м', N: 'Н', n: 'н', O: 'О', o: 'о', P: 'П', p: 'п', R: 'Р', r: 'р', S: 'С', s: 'с', T: 'Т', t: 'т', Ć: 'Ћ', ć: 'ћ', U: 'У', u: 'у', F: 'Ф', f: 'ф', H: 'Х', h: 'х', C: 'Ц', c: 'ц', Č: 'Ч', č: 'ч', Š: 'Ш', š: 'ш' }
+const chars = { Lj: 'Љ', lj: 'љ', Nj: 'Њ', nj: 'њ', Dž: 'Џ', dž: 'џ', A: 'А', a: 'а', B: 'Б', b: 'б', V: 'В', v: 'в', G: 'Г', g: 'г', D: 'Д', d: 'д', Đ: 'Ђ', đ: 'ђ', E: 'Е', e: 'е', Ž: 'Ж', ž: 'ж', Z: 'З', z: 'з', I: 'И', i: 'и', J: 'Ј', j: 'ј', K: 'К', k: 'к', L: 'Л', l: 'л', M: 'М', m: 'м', N: 'Н', n: 'н', O: 'О', o: 'о', P: 'П', p: 'п', R: 'Р', r: 'р', S: 'С', s: 'с', T: 'Т', t: 'т', Ć: 'Ћ', ć: 'ћ', U: 'У', u: 'у', F: 'Ф', f: 'ф', H: 'Х', h: 'х', C: 'Ц', c: 'ц', Č: 'Ч', č: 'ч', Š: 'Ш', š: 'ш' }
 const lock = [
   'meta.title',
   'where.links.gpx',
   'where.links.gmaps',
   'when.age',
   'when.descriptionA',
-  'rsvp.title'
+  'rsvp.title',
 ]
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const path = type => join(__dirname, '../locales', `sr-${type}-RS.yml`)
-
 const latn = parse(fs.readFileSync(path('Latn'), 'utf8'))
 const orig = parse(fs.readFileSync(path('Cyrl'), 'utf8'))
 const cyrl = recursiveReplace(latn)
@@ -25,7 +24,7 @@ function recursiveReplace(obj, pk) {
   const result = {}
   for (const key in obj) {
     const parentKey = pk ? `${pk}.${key}` : key
-    if (pk && lock.includes(parentKey))
+    if (lock.includes(parentKey))
       result[key] = get(orig, parentKey)
     else if (typeof obj[key] === 'string')
       result[key] = convert(obj[key])
@@ -37,8 +36,8 @@ function recursiveReplace(obj, pk) {
 
 function convert(str) {
   return str.replace(
-    new RegExp(Object.keys(map).join('|'), 'gi'),
-    match => map[match],
+    new RegExp(Object.keys(chars).join('|'), 'gi'),
+    match => chars[match],
   )
 }
 
