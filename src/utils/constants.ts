91c9dcf73633fccb0a191b876aedfbd81a10d0bd
Goran <gospaso@gmail.com>
2024-04-08 10:26:51 -0400

    paypal link

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,4 +3,4 @@ export const IMAGES = Array.from({ length: 35 }, (_, i) => `/media/md/${i + 1}.j
 export const VIDEOS = Array.from({ length: 7 }, (_, i) => `/media/lg/${99 - i}.mp4`)
 
 export const venmo = 'https://venmo.com/u/Armageddion'
-export const paypal = 'https://www.paypal.com/qrcodes/p2pqrc/VZAZRNJESBTX2'
+export const paypal = 'https://www.paypal.me/armageddion'
