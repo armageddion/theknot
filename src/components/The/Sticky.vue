5e249ba297ed59b1eaa67bc5e569eb25730e9d91
Goran <gospaso@gmail.com>
2024-05-07 08:47:36 +0200

    wip: make sticky rsvp glow

--- src/components/The/Sticky.vue
+++ src/components/The/Sticky.vue
@@ -3,11 +3,40 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <a
-    fixed bottom-8 z-1 flex items-center justify-center gap-2 btn
-    left="1/2" translate-x="-1/2"
-    @click="goTo('#rsvp')"
+  <aside
+    fixed bottom-4 z-1
+    right="4 sm:1/2" translate-x="sm:1/2"
+    class="group"
   >
-    {{ t('rsvp.title') }}
-  </a>
+    <div class="glow" group-hover:opacity-100 />
+    <a
+      btn
+      @click="goTo('#rsvp')"
+    >
+      {{ t('rsvp.title') }}
+    </a>
+  </aside>
 </template>
+
+<style scoped>
+.glow {
+  position: absolute;
+  left: 50%;
+  top: 0;
+  transform: translate(-50%, -50%);
+  width: 160px;
+  height: 160px;
+  opacity: 0;
+  transition: all 1s ease;
+  pointer-events: none;
+  z-index: -1;
+  border-radius: 9999px;
+  background-image: linear-gradient(
+    45deg,
+    var(--color-secondary),
+    var(--color-secondary),
+    var(--color-accent)
+  );
+  filter: blur(60px);
+}
+</style>
