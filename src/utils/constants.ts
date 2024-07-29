b1a7de08fa0cfc9715566c8178128dba49639878
armageddion <armageddion@gmail.com>
2024-07-29 11:38:46 +0200

    update img count 35->39

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 export const SECTIONS = ['welcome', 'where', 'when', 'what', 'gift', 'rsvp'] as const
-export const IMAGES = Array.from({ length: 35 }, (_, i) => `/media/md/${i + 1}.jpg`)
+export const IMAGES = Array.from({ length: 39 }, (_, i) => `/media/md/${i + 1}.jpg`)
 export const VIDEOS = Array.from({ length: 7 }, (_, i) => `/media/lg/${99 - i}.mp4`)
 
 export const venmo = 'https://venmo.com/u/Armageddion'
